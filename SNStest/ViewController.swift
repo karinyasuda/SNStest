03a502eeb984a7342068c74cfc78fb3ea041bc06
Karin <karin.03k35@gmail.com>
2016-03-10 17:16:19 +0900

    Initial Commit

---
+++ SNStest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SNStest
+//
+//  Created by Karin on 2016/03/10.
+//  Copyright © 2016年 Karin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
